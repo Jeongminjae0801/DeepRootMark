fef9d061dd1f24229cafe282f78fc8e985cdd8a7
minjae <aimar16a@gmail.com>
2018-04-24 17:10:02 +0900

    0424_Spring

---
+++ 민재/SpringLabs/SpringDI_Basic02_xmlsetting/src/DI_09_Spring/ProtocolHandlerFactory.java
@@ -0,0 +1,17 @@
+package DI_09_Spring;
+
+import java.util.Map;
+
+public class ProtocolHandlerFactory {
+
+	// Map(key, value)
+	private Map<String, ProtocolHandler> handler;
+
+	public void setHandler(Map<String, ProtocolHandler> handler) {
+		this.handler = handler;
+		System.out.println("setter 주입 성공 :: " + this.handler);
+	}
+	
+	
+	
+}
