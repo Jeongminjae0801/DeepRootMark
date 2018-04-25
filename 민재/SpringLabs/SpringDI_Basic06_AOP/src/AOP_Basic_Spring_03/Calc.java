387b92c8f3ce2269fe262925475f49c7c091990a
minjae <aimar16a@gmail.com>
2018-04-25 16:57:59 +0900

    0425 Spring2

---
+++ 민재/SpringLabs/SpringDI_Basic06_AOP/src/AOP_Basic_Spring_03/Calc.java
@@ -0,0 +1,11 @@
+package AOP_Basic_Spring_03;
+
+public interface Calc {
+
+	int Add(int x, int y);
+	
+	int Mul(int x, int y);
+	
+	int sub(int x, int y);
+	
+}
---
+++ 민재/SpringLabs/SpringDI_Basic06_AOP/src/AOP_Basic_Spring_03/Program.java
@@ -0,0 +1,19 @@
+package AOP_Basic_Spring_03;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		// Proxy 객체를 생성하고 Proxy를 통해서 함수를 실행
+		// Spring xml 설정을 통해서
+		ApplicationContext context = 
+				new GenericXmlApplicationContext("classpath:AOP_Basic_Spring_03/ApplicationContext.xml");
+		
+		Calc calc = context.getBean("proxy",Calc.class);
+		int result = calc.Add(10, 20);
+		System.out.println(result);
+	}
+}
