0c98b9c2df8e7eb46eb11d998bd6723b74e4b19d
khj <gimijuny@gmail.com>
2018-05-01 20:12:57 +0900

    aop xml 추가

--- AOP/SpringMVC_Z_AOP/src/kr/or/bit/aspect/AspectJ.java
+++ AOP/SpringMVC_Z_AOP/src/kr/or/bit/aspect/AspectJ.java
@@ -7,6 +7,7 @@ import java.util.Date;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -25,6 +26,20 @@ public class AspectJ {
 	public void before(JoinPoint joinpoint) {
 		Log log =	LogFactory.getLog(this.getClass());
 		log.info("Before");
+		long time = System.currentTimeMillis(); 
+
+		SimpleDateFormat dayTime = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
+
+		String date = dayTime.format(new Date(time));
+
+		log.info(date); // 현재 시간 찍기
+		log.info(joinpoint.getSignature().getName()); //MainController에서 부른 함수이름 찍기
+		
+	}
+	
+	public void after(JoinPoint joinpoint) {
+		Log log =	LogFactory.getLog(this.getClass());
+		log.info("After");
 		long time = System.currentTimeMillis(); 
 
 		SimpleDateFormat dayTime = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
