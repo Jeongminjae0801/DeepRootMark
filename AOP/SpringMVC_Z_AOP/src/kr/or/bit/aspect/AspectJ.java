43171578268618c5619b9c67b221e2cd4ede3e2c
khj <gimijuny@gmail.com>
2018-05-01 12:17:01 +0900

    before 적용 (시간이랑 누가 불렀는지 로그 찍기)

--- AOP/SpringMVC_Z_AOP/src/kr/or/bit/aspect/AspectJ.java
+++ AOP/SpringMVC_Z_AOP/src/kr/or/bit/aspect/AspectJ.java
@@ -1,8 +1,10 @@
 package kr.or.bit.aspect;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
+import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
@@ -15,19 +17,22 @@ import org.springframework.web.HttpRequestHandler;
 @Aspect
 public class AspectJ {
 
-	@Pointcut("execution(* kr.or.bit.controller.MainController.*(..))")
+	@Pointcut("execution(* kr.or.bit.controller.MainController.*(..))") //MainController에 있는 모든 함수
 	public void pointcut() {}
 	
-	@Before("pointcut()")
-	public Object before(JoinPoint joinpoint) {
 	
-		Date date = Calendar.getInstance().getTime();
-		String time = date.getTime() + "-" + date.getHours() + "-" + date.getMinutes();
-		
-		System.out.println("before: ");
-		
-		
-		return null;
+	@Before("pointcut()")
+	public void before(JoinPoint joinpoint) {
+		Log log =	LogFactory.getLog(this.getClass());
+		log.info("Before");
+		long time = System.currentTimeMillis(); 
+
+		SimpleDateFormat dayTime = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
+
+		String date = dayTime.format(new Date(time));
+
+		log.info(date); // 현재 시간 찍기
+		log.info(joinpoint.getSignature().getName()); //MainController에서 부른 함수이름 찍기
 		
 	}
 }
