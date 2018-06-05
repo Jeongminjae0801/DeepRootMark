5fcce09a931d7b0e3a4cd34cf9744850dee2afe9
khj <gimijuny@gmail.com>
2018-06-05 11:19:32 +0900

    Spring Project Setting

---
+++ Final/DeepRoot/src/main/java/kr/or/bit/controller/MainController.java
@@ -0,0 +1,8 @@
+package kr.or.bit.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MainController {
+
+}
---
+++ Final/DeepRoot/src/main/java/kr/or/bit/service/UserService.java
@@ -0,0 +1,8 @@
+package kr.or.bit.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+}
