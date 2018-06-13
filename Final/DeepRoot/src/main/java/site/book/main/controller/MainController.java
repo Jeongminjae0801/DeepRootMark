8a90b6df9f400616b0007cefa7e635fb84e37a48
khj <gimijuny@gmail.com>
2018-06-13 12:28:50 +0900

    미리보기 back단 완료

--- Final/DeepRoot/src/main/java/site/book/main/controller/MainController.java
+++ Final/DeepRoot/src/main/java/site/book/main/controller/MainController.java
@@ -8,13 +8,22 @@
 
 package site.book.main.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.View;
 
 import site.book.admin.dto.A_BookDTO;
 import site.book.admin.dto.A_CategoryDTO;
@@ -40,7 +49,8 @@ public class MainController {
 	private A_BookService a_book_service;
 	
 	// 희준
-	
+	@Autowired
+	private View jsonview;
 	
 	
 	// 명수
@@ -76,7 +86,47 @@ public class MainController {
 	/* Roll in */
 	
 	// 희준
-	
+	@RequestMapping("webcrawling.do")
+	public View WebCrawling(String url, Model model) {
+		Document doc;
+		Map<String, List<String>> result = new HashMap<String, List<String>>();
+		String[] REQUIRED_META = new String[] { "og:title", "og:type", "og:image", "og:url", "og:description" };
+		try {
+			doc = Jsoup.connect(url).userAgent(
+					"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36")
+					.referrer("http://www.google.com").get();
+			Elements ogElements = doc.select("meta[property^=og], meta[name^=og]");
+			for (Element e : ogElements) {
+				String target = e.hasAttr("property") ? "property" : "name";
+
+				if (!result.containsKey(e.attr(target))) {
+					result.put(e.attr(target), new ArrayList<String>());
+				}
+				result.get(e.attr(target)).add(e.attr("content"));
+			}
+			for (String s : REQUIRED_META) {
+				if (!(result.containsKey(s) && result.get(s).size() > 0)) {
+					if (s.equals(REQUIRED_META[0])) {
+						result.put(REQUIRED_META[0], Arrays.asList(new String[] { doc.select("title").eq(0).text() }));
+					} else if (s.equals(REQUIRED_META[1])) {
+						result.put(REQUIRED_META[1], Arrays.asList(new String[] { "website" }));
+					} else if (s.equals(REQUIRED_META[2])) {
+						result.put(REQUIRED_META[2],
+								Arrays.asList(new String[] { doc.select("img").eq(0).attr("abs:src") }));
+					} else if (s.equals(REQUIRED_META[3])) {
+						result.put(REQUIRED_META[3], Arrays.asList(new String[] { doc.baseUri() }));
+					} else if (s.equals(REQUIRED_META[4])) {
+						result.put(REQUIRED_META[4], Arrays.asList(new String[] { doc
+								.select("meta[property=description], meta[name=description]").eq(0).attr("content") }));
+					}
+				}
+			}
+			for (String s : result.keySet()) {model.addAttribute(s.substring(3), result.get(s));}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return jsonview;
+	}
 	
 	
 	
