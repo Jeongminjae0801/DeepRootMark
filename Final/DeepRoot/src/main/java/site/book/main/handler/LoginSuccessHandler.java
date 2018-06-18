9add2428b523b2fb33d681bbb84f57e5257a2a7e
woong <ktaewoong46@gmail.com>
2018-06-18 10:09:55 +0900

    Back end: 관리자 로그인시 관리자 페이지로!!

--- Final/DeepRoot/src/main/java/site/book/main/handler/LoginSuccessHandler.java
+++ Final/DeepRoot/src/main/java/site/book/main/handler/LoginSuccessHandler.java
@@ -1,6 +1,7 @@
 package site.book.main.handler;
 
 import java.io.IOException;
+import java.util.Collection;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -9,6 +10,8 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
 import org.springframework.security.web.savedrequest.RequestCache;
@@ -31,6 +34,19 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler {
 		// authentication.getName(): userid
 		request.setAttribute("userid", authentication.getName());
 		request.setAttribute("msg", "success");
+		
+		Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+		boolean authorized = authorities.contains(new SimpleGrantedAuthority("ROLE_ADMIN"));
+		
+		//System.out.println(authorized);
+		
+		if(authorized) {
+			System.out.println("????");
+			request.setAttribute("ROLE", "ADMIN");
+		}else {
+			request.setAttribute("ROLE", "USER");
+		}
+		
 		request.getRequestDispatcher("/joinus/login.do").forward(request, response);
 				
 	}
--- Final/DeepRoot/src/main/webapp/js/login.js
+++ Final/DeepRoot/src/main/webapp/js/login.js
@@ -74,6 +74,56 @@ function shakeModal_login() {
         $('#loginModal .modal-dialog').removeClass('shake');
     }, 1000);
 }
+
+/*
+ * 회원가입 유효성 확인
+ */
+$(function() {
+    //비밀번호 길이 확인 함수
+    $('#pwd_join').keyup(function() {
+        if (($('#pwd_join').val().trim() == "") || !($('#pwd_join').val().length >= 5 && $('#pwd_join').val().length <= 15)) {
+            $('.error').addClass('alert alert-danger').html("비밀번호는 5자~15자 사이로 만들어야 합니다.");
+        } else {
+            $('.error').removeClass('alert alert-danger').html('');
+        }
+    });
+
+    //비밀번호 동일 확인 함수
+    $('#pwd_confirmation').keyup(function() {
+        /*var pwd = $('#pwd_join').val();
+        var pwd_con = $('#pwd_confirmation').val();*/
+        if (!($('#pwd_join').val() == $('#pwd_confirmation').val())) {
+            $('.error').addClass('alert alert-danger').html("입력한 비밀번호가 다릅니다.");
+        } else {
+            $('.error').removeClass('alert alert-danger').html('');
+        }
+    });
+    
+    //이메일 형식 확인 함수
+    $('#uid_join').keyup(function() {
+        /*var email = $('#uid_join').val();*/
+        var regex = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/; 
+        if(($('#uid_join').val().trim() == "") || !($('#uid_join').val().length >= 5 )){
+                 $('.error').addClass('alert alert-danger').html("이메일은 5자 이상 입력해야 합니다.");
+            
+        } else if( !(regex.test($('#uid_join').val())) ) {
+                 $('.error').addClass('alert alert-danger').html("형식에 맞지 않은 이메일 입니다.");
+        } else {
+            $('.error').removeClass('alert alert-danger').html('');
+        }
+    });
+    
+    //인증키 확인 함수
+    $('#authcode_join').keyup(function(){
+        if( ($('#authcode_join').val().length >= 11 ) ) {
+            $('.error').addClass('alert alert-danger').html("형식에 맞지 않은 인증키 입니다.");
+        }
+        else{
+            $('.error').removeClass('alert alert-danger').html('');
+        }
+    });
+})
+
 /*
  * 회원가입 Ajax + 유효성 확인
  */
@@ -351,8 +401,8 @@ $(function() {
             dataType:"json",
            /*  crossDomain: false, */
             success:function(data){
-            	if(data.login == 'success') { 
-            		location.href="index.do";
+            	if(data.login == 'success') {
+            		location.href = data.path;
             	}
             	else {  
 	            	$("#pwd").val('');
